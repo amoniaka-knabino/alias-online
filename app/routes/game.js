be28ab0827efd4885dc86035b59170eaad8e94d7
amoniaka-knabino <sv@dynfor.ru>
2020-06-24 12:43:16 +0700

    create game api method

--- app/routes/game.js
+++ app/routes/game.js
@@ -1,22 +1,31 @@
 const cookieParser = require("cookie-parser");
-//var app = require('../app');
-
 
 var express = require('express');
 var router = express.Router();
 
 //app.use(cookieParser());
 //const Round = require('../classes/round-class.js');
-//const Player = require('../classes/player-class.js');
+const Player = require('../classes/player-class.js');
 const Game = require('../classes/game-class.js');
 
-router.get('/join_game', function(req, res) {
+router.get('/join', function(req, res) {
     var inviteToken = req.query.inviteToken;
     console.log(inviteToken);
-    //req.query("inviteToken");
     var userToken = req.cookies["userToken"];
     console.log(userToken);
     res.json([inviteToken, userToken]);
+    //get game from db, add player, save to db...
+});
+
+router.get('/create', function(req, res) {
+    var userToken = req.cookies["userToken"];
+    var userName = req.cookies["userName"];
+    var roundTime = req.query.roundTime;
+    var roundCount = req.query.roundCount;
+    var fisrtPlayer = new Player(userName, userToken);
+    var game = new Game(roundCount, roundTime, fisrtPlayer);
+    console.log(game)
+    res.json({"inviteToken":game.InviteToken});
 });
 
 
