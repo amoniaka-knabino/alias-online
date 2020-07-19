24689e3842686cc653b08c3742f62cf682eb0dfd
somnoynadno <somnoynadno@yandex.ru>
2020-07-19 09:20:49 +0500

    var -> let + some typos fixed

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 const Round = require('../classes/round-class.js');
 const Player = require('../classes/player-class.js');
